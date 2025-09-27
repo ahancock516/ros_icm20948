bc82e6aeed53b51167c7bdab0baa48ddcb132309
ahancock516 <hancockaaron16@gmail.com>
2025-09-27 15:00:35 -0400

    Renamed talker.py to icm20948_node.py

--- src/icm20948_node.py
+++ src/icm20948_node.py
@@ -25,33 +25,33 @@ def icm20948_node():
         message = "The Qwiic ICM20948 device isn't connected to the system. Please check your connection"
         rospy.loginfo(message)
 
-    IMU.begin()    
+    IMU.begin()
 
     while not rospy.is_shutdown():
         if IMU.dataReady():
             IMU.getAgmt()
             raw_msg = Imu()
             raw_msg.header.stamp = rospy.Time.now()
-	            
+
             raw_msg.orientation.w = 0
             raw_msg.orientation.x = 0
             raw_msg.orientation.y = 0
             raw_msg.orientation.z = 0
-                
+
             raw_msg.linear_acceleration.x = IMU.axRaw
             raw_msg.linear_acceleration.y = IMU.ayRaw
             raw_msg.linear_acceleration.z = IMU.azRaw
-                
+
             raw_msg.angular_velocity.x = IMU.gxRaw
             raw_msg.angular_velocity.y = IMU.gyRaw
             raw_msg.angular_velocity.z = IMU.gzRaw
-                
+
             raw_msg.orientation_covariance[0] = -1
             raw_msg.linear_acceleration_covariance[0] = -1
             raw_msg.angular_velocity_covariance[0] = -1
-                
+
             raw_pub.publish(raw_msg)
-                
+
             mag_msg = MagneticField()
             mag_msg.header.stamp = rospy.Time.now()
             mag_msg.magnetic_field.x = IMU.mxRaw
@@ -60,8 +60,8 @@ def icm20948_node():
             mag_msg.magnetic_field_covariance[0] = -1
             mag_pub.publish(mag_msg)
 
-        rate.sleep()   
-    
+        rate.sleep()
+
     rospy.loginfo(rospy.get_caller_id() + "  icm20948 node finished")
 
 if __name__ == '__main__':
